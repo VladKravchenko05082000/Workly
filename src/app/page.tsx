c818510821e19378078d83e0b557e0206d9bc094
VladKravchenko05082000 <daitevoitipozalysta@gmail.com>
2026-04-26 15:59:02 +0200

    remove mui library imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import NavLink from "@/components/navigate/NavLink";
-
-import { routeConfig } from "@/lib/constants/routeConfig";
-import { Button } from "@mui/material";
-
-export default function Home() {
-  return (
-    <div className="bg-bg-surface2">
-      <NavLink href={`/${routeConfig.home}`}>To home</NavLink>
-      <Button variant="contained">Hello world</Button>
-    </div>
-  );
+export default function Init() {
+  return <div className="bg-bg-surface2"></div>;
 }
